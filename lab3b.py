23763509a522db2a4cdbc706da980dec14382b63
Devika Karumathil Chaudhuri <devika@lnxsrv09.seas.ucla.edu>
2018-05-27 15:23:10 -0700

    started the format

--- lab3b.py
+++ lab3b.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python
+
+import csv
+
+
+inode_list = list()
+
+
+class Inodes:
+    def __init__(self, number):
+        self.number = number;
+        self.links_to_me = 0;
+        self.on_free_list = -1;#0 means no, 1 means yes
+        print("hi\n")
+    
+
+
+def csv_dict_reader(file_obj):
+    reader = csv.reader(file_obj, delimiter=",")
+    for row in reader:
+        if row[0] == "IFREE":
+            i = Inodes(row[1])
+            i.on_free_list = 1
+            inode_list.append(i)xs
+            
+
+if __name__ == "__main__":
+    with open("trivial.csv") as f_obj:
+              csv_dict_reader(f_obj)
+    print(inode_list[3].number)
