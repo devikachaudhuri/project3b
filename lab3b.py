72f4e7558233c6c1fa28c556d8da130625611ba8
Devika Karumathil Chaudhuri <devika@lnxsrv09.seas.ucla.edu>
2018-05-27 20:16:04 -0700

    allocated inode on free list now works yay

--- lab3b.py
+++ lab3b.py
@@ -5,6 +5,8 @@ import sys
 
 inode_list = list()
 inode_numbers = list() #list of just inode numbers
+free_inode = list ()
+alloc_inode = list()
 unallocated_and_not_on_free_list = list () #list of un
 dir_list = list()
 superblock = list()
@@ -19,8 +21,6 @@ class Inodes:
     def __init__(self, number):
         self.number = number
         self.links_to_me = 0
-        self.on_free_list = -1 #0 means no, 1 means yes
-        self.allocated = -1
         self.recorded_link_count = 0
 
 class Directories:
@@ -33,17 +33,21 @@ class Directories:
 ########################HELPER FUNCTIONS#################################
 
 def is_on_free_list(num):
-    for item in inode_list:
-        if item.number == num and item.on_free_list == "1":
-            return 1
+    if num in free_inode:
+        return 1
+    else:
+        return 0
+
+def is_on_alloc_list(num):
+    if num in alloc_inode:
+        return 1
+    else:
+        return 0
         
 def update_inode_link_count():
-#    print "amount " + str(len(inode_list))
-    for dir in dir_list:
-        linked_ino = dir.file_inode
+    for dire in dir_list:
+        linked_ino = dire.file_inode
         for item in inode_list:
-            #       print "item number " + item.number
-            #      print "linked_ino " + linked_ino
             if item.number == linked_ino:
                 item.links_to_me = item.links_to_me + 1
                 
@@ -70,25 +74,23 @@ def check_inodes():
     is_there_unallocated_inodes()
     for x in unallocated_and_not_on_free_list:
         print "UNALLOCATED INODE " + str(x) + " NOT ON FREELIST"
+    for x in free_inode:
+        if x in alloc_inode:
+            print "ALLOCATED INODE " + x + " ON FREELIST"
     for item in inode_list:
-        #check allocated vs unallocated
-        if item.on_free_list == "1" and item.allocated == "1":
-            print "ALLOCATED INODE " + item.number + " ON FREELIST"
-        #if item.on_free_list == "0" and item.allocated == "0":
-         #   print "UNALLOCATED INODE " + item.number + " NOT ON FREELIST"
-        #check link counts match
         if str(item.recorded_link_count) != str(item.links_to_me):
-            print "INODE " + item.number + " HAS " + str(item.recorded_link_count) + " LINKS BUT LINKCOUNT IS " + str(item.links_to_me)
+            if item.number not in free_inode:
+                print "INODE " + item.number + " HAS " + str(item.recorded_link_count) + " LINKS BUT LINKCOUNT IS " + str(item.links_to_me)
 
 def check_directories():
     update_previous_inodes()
     for item in dir_list:
         #check for invalid inodes
         if item.file_inode < 1 or item.file_inode > superblock[0].num_inodes:
-            print "DIRECTORY INODE " + item.parent_inode + " NAME '" + dir_list.name + "' INVALID INODE " + item.file_inode
-        #check for unallocate inodes
-        if (is_on_free_list(item.file_inode)):
-            print "DIRECTORY INODE " + item.parent_inode + " NAME '" + dir_list.name + "' UNALLOCATED INODE " + item.file_inode
+            print "DIRECTORY INODE " + item.parent_inode + " NAME '" + item.name + "' INVALID INODE " + item.file_inode
+        #check for unallocated inodes
+        if (is_on_alloc_list(item.file_inode) == 0):
+            print "DIRECTORY INODE " + item.parent_inode + " NAME '" + item.name + "' UNALLOCATED INODE " + item.file_inode
         #check for . (itself) and .. (previous inode) correct linking
         if item.name == "." and item.parent_inode != item.file_inode:
             print "DIRECTORY INODE " + item.parent_inode + " NAME '.' LINK TO INODE " + item.file_inode + " SHOULD BE " + item.parent_inode
@@ -101,21 +103,21 @@ def csv_dict_reader(file_obj):
     for row in reader:
         if row[0] == "IFREE": #put the free inodes on the inode list
             i = Inodes(row[1])
-            i.on_free_list = 1
             n = row[1]
             inode_numbers.append(n)
+            free_inode.append(n)
             inode_list.append(i)
         if row[0] == "INODE": #put the allocated inodes on the inode list
             k = Inodes(row[1])
-            k.allocated = 1
             k.recorded_link_count = row[6]#record the link count
             n = row[1]
             inode_numbers.append(n)
+            alloc_inode.append(n)
             inode_list.append(k)
         if row[0] == "DIRENT":
             d = Directories(row[1], row[3], row[6])
-            #update_inode_link_count(row[3])
             dir_list.append(d)
+            #update_inode_allocation(row[1])
         if row[0] == "SUPERBLOCK":
             s = Super()
             s.num_inodes = row[2]
